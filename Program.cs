af296925b62d3b7a797e4d53afb3f8cfdb484801
Vaxobov Azamat <azamatvaxobov104@gmail.com>
2024-06-05 12:35:28 +0300

    Changed to right version

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿﻿// See https://aka.ms/new-console-template for more information
-decimal number1 = 45;
-decimal number2 = 47;
+decimal number1, number2;
+
+Console.Write( "Enter number1 : " );
+number1 = Convert.ToDecimal( Console.ReadLine() );
+
+Console.Write( "Enter number2 : " );
+number2 = Convert.ToDecimal( Console.ReadLine() );
 
 Console.WriteLine( $"{number1} * {number2} = {number1*number2}" );
 Console.WriteLine( $"{number1} - {number2} = {number1-number2}" );
 Console.WriteLine( $"{number1} / {number2} = {number1/number2}" );
 Console.WriteLine( $"{number1} + {number2} = {number1+number2}" );
+Console.WriteLine( $"{number1} % {number2} = {number1%number2}" );
