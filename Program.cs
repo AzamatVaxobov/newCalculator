cf24c910916aeb8113376c0c481b771cb56ba1fe
Vaxobov Azamat <azamatvaxobov104@gmail.com>
2024-06-05 10:51:55 +0300

    Add project

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿﻿// See https://aka.ms/new-console-template for more information
+decimal number1 = 45;
+decimal number2 = 47;
+
+Console.WriteLine( $"{number1} * {number2} = {number1*number2}" );
+Console.WriteLine( $"{number1} - {number2} = {number1-number2}" );
+Console.WriteLine( $"{number1} / {number2} = {number1/number2}" );
+Console.WriteLine( $"{number1} + {number2} = {number1+number2}" );
